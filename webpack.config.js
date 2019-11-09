86380c4c34a31dc89d0d45b45f65cc71dadd580f
Guillermo Garcia Jr <guillermo.jr23@outlook.com>
2019-11-08 18:16:13 -0800

    Working on front-end, landing page..

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,21 @@ module.exports = {
         rules:
             [{
                 test: /\.(s*)css$/,
-                use: ['style-loader', 'css-loader','sass-loader']
-            }]
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true, // webpack@1.x
+                            disable: true, // webpack@2.x and newer
+                        },
+                    },
+                ],
+            }
+            ]
     }
 };
